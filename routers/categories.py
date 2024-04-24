a201e235d49a6c1586e45337577449af7464512d
ina-ene <ina.ene.pancake@gmail.com>
2024-04-24 16:57:28 +0300

    added response

--- routers/categories.py
+++ routers/categories.py
@@ -3,8 +3,7 @@ from data.models import Category, CategoryResponseModel
 from services import category_service as cs
 from services import topic_service as ts
 from common.auth import get_user_or_raise_401
-from common.responses import Forbidden
-
+from common.responses import Forbidden, NotFound
 
 
 categories_router = APIRouter(prefix="/categories")
@@ -25,7 +24,7 @@ def get_category_by_id(id: int):
     category = cs.get_by_id(id)
 
     if category is None:
-        return "there is no such category"
+        return NotFound("This category does not exist!")
     else:
         return CategoryResponseModel(
             category=category, topics=ts.get_by_category(category.id)
@@ -33,7 +32,7 @@ def get_category_by_id(id: int):
 
 
 @categories_router.post("/", status_code=201)
-def create_category(category: Category, x_token: str = Header() ):
+def create_category(category: Category, x_token: str = Header()):
     user = get_user_or_raise_401(x_token)
     if not user.is_admin():
         return Forbidden("You are not admin!")
