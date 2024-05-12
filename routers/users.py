cf280307a5e145922711d4355e2db3bbcbf88acf
ina-ene <ina.ene.pancake@gmail.com>
2024-05-12 17:06:32 +0300

    updating users

--- routers/users.py
+++ routers/users.py
@@ -1,14 +1,22 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Request, Depends, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from data.models import LoginData, User
 from services import user_service
 from common import responses, auth
 
 
 users_router = APIRouter(prefix='/users')
+templates = Jinja2Templates(directory="templates")
+
+@users_router.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
 
 
 @users_router.post("/register")
-def register_user(user: User):
+async def register_user(user: User = Depends(User.from_form)):
     if user_service.check_if_username_exists(user.username):
         return responses.BadRequest("Username is taken")
     if user_service.check_if_email_exists(user.email):
@@ -16,9 +24,14 @@ def register_user(user: User):
     return user_service.create(user)
 
 
+@users_router.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
 @users_router.post('/login')
-def login(data: LoginData):
-    user = user_service.find_by_username_password(data.username, data.password)
+def login(username = Form(...), password= Form(...)):
+    user = user_service.find_by_username_password(username, password)
 
     if user:
         token = auth.create_token(user)
